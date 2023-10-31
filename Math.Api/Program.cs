b96aecfc7ab29542cb9209aa6234dd983c94e933
Muqimjon <muqimjon.mr@gmail.com>
2023-10-31 17:11:26 +0500

    Dockerize job added

--- Math.Api/Program.cs
+++ Math.Api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddScoped<IMathService, MathService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
